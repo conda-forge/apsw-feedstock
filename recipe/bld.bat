20a2da04763fecff329f972f8b2bc5929195ca22
Jan Stürtz <jan.stuertz@contact-software.com>
2022-09-05 16:48:56 +0200

    fix: set SQLITE_ENABLE_COLUMN_METADATA=1 as in the sqlite build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,4 @@
+set CFLAGS="-DSQLITE_ENABLE_COLUMN_METADATA=1 %CFLAGS%"
 %PYTHON% setup.py build --enable-all-extensions --enable=load_extension
 %PYTHON% setup.py install --single-version-externally-managed --record record.txt
 %PYTHON% setup.py test
