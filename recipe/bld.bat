f1e600674ad1b0d4171aaa15e639e6c97c300bc9
Jan Stürtz <jan.stuertz@contact-software.com>
2022-09-05 17:18:10 +0200

    fix: windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-set CFLAGS="-DSQLITE_ENABLE_COLUMN_METADATA=1 %CFLAGS%"
+set "CL= -DSQLITE_ENABLE_COLUMN_METADATA=1"
 %PYTHON% setup.py build --enable-all-extensions --enable=load_extension
 %PYTHON% setup.py install --single-version-externally-managed --record record.txt
 %PYTHON% setup.py test
