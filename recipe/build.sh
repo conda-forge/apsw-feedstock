51fdda03a0b987714c816cb8dfcd321ff453b925
Lori A. Burns <lori.burns@gmail.com>
2024-07-25 11:51:32 -0400

    no test silicon

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,4 +3,7 @@ export CFLAGS="-I$PREFIX/include -DSQLITE_ENABLE_COLUMN_METADATA=1 ${CFLAGS}"
 export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib ${LDFLAGS}"
 $PYTHON setup.py build --enable-all-extensions --enable=load_extension
 $PYTHON setup.py install --single-version-externally-managed --record record.txt
-$PYTHON setup.py test
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+    $PYTHON setup.py test
+fi
